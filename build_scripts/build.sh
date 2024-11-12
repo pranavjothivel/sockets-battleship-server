8c18008361abae8e3fd50f1715f1a10c0622f574
Pranav Jothivel <44120295+pranavjothivel@users.noreply.github.com>
2024-11-12 17:42:10 +0000

    update build script

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -6,13 +6,21 @@ mkdir -p build
 # Array of source files
 sources=("hw4.c" "player_automated.c" "player_interactive.c")
 
+# Check if specific files were provided as arguments
+if [ "$#" -gt 0 ]; then
+    # Use command-line arguments as the source files
+    sources=("$@")
+else
+    echo "No specific files provided. Compiling all default sources."
+fi
+
 # Loop through each source file and compile it
 for src in "${sources[@]}"; do
     # Extract the base filename without extension
     base_name=$(basename "$src" .c)
     # Compile the file with debugging information, outputting to the build directory
-    gcc -g "src/$src" -o "build/$base_name"
-    # echo "Compiled $src to build/$base_name"
+    gcc -g "./src/$src" -o "build/$base_name"
+    echo "Compiled $src to build/$base_name"
 done
 
 echo "Compilation complete."
